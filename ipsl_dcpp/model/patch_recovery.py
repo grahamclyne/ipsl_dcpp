bug in normalization for smoothing

ipsl_dcpp/model/patch_recovery.py
@@ -217,7 +217,7 @@ class PatchRecovery4(nn.Module):
                       upscale_factor=upscale_factor)
         self.smooth_conv = nn.Conv2d(output_dim, output_dim, kernel_size=5, stride=1, padding=2)
         self.smooth_conv1 = nn.Conv2d(output_dim, output_dim, kernel_size=3, stride=1, padding=1)
-        self.norm = nn.LayerNorm(output_dim)
+        self.norm = nn.LayerNorm((output_dim,144,144))
         self.conv.weight.data.copy_(weight)   # initialize conv.weight
 
     def forward(self, x):